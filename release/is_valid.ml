040eff2143df69873b47fe23588d8b827eb43c84
Kavan Bhavin <kab395@cornell.edu>
2014-04-27 21:48:59 -0400

    alksdjfh

--- release/is_valid.ml
+++ release/is_valid.ml
@@ -1,4 +1,5 @@
 open Util
+open Definition
 
 (* CHECK VALID INITIAL MOVES *)
 
@@ -23,3 +24,24 @@ let free_valid_pair (p1, p2) (intersections, roads) : bool =
   (valid_pair (p1, p2)) && 
     (empty_road (p1, p2) roads) &&
     (empty_settlement p1 intersections)
+
+let is_valid_piece piece = 
+  piece>=0 && piece<= 18
+
+  (* Given a settlement, return a valid road leaving it,
+   or None if this is impossible. *)
+let get_valid_road settle structs color: road option = 
+  let corner_pairs = List.map (fun v -> (settle, [v])) (adjacent_points settle) in
+  let good_pair = 
+    try Some (List.find (fun (start_loc, adj) -> try ignore (List.find (fun end_loc ->
+     free_valid_pair (start_loc, end_loc) structs) adj); true
+     with Not_found -> false) corner_pairs) with Not_found -> None in
+  match good_pair with
+  | Some (start_loc, adj) ->
+    let end_loc = try (List.find (fun end_loc ->
+     free_valid_pair (start_loc, end_loc) structs) adj) with
+        Not_found -> failwith "Get_Valid_Road failed to remove invalid start points!" in
+    Some (color, (start_loc, end_loc))
+  | None -> None
+
+  
