2da5f10b858d7d82fab4f55046798575a9f4aee2
Kavan Bhavin <kab395@cornell.edu>
2014-04-29 00:12:25 -0400

    finished first part of RollDice action

--- release/action.ml
+++ release/action.ml
@@ -2,12 +2,38 @@ open Util
 open Definition 
 open Constant
 open Own_util
-let doAction action = 
+
+let resources_of_corners (resource: resource) (acc: player list) (point: point) (intersections, roads) : player list = 
+	match List.nth intersections point with 
+	| Some (color, settlement) -> 
+	let to_be_added = (map_cost (fun num -> num*(settlement_num_resources settlement)) (single_resource_cost resource))
+	in List.map (fun (pcolor, (inventory, cards), trophies) -> 
+		if pcolor= color then 
+let new_inventory = sum_of_two_costs inventory to_be_added
+in (pcolor, (new_inventory, cards), trophies)
+else (pcolor, (inventory, cards), trophies)) acc
+	| None -> acc
+
+
+let generate_resources (roll: roll) ((map: map), (structures: structures), (deck: deck), (discard: discard), (robber: robber)) (plist: player list) = 
+	let hexes_with_corners = List.mapi (fun index (terrain, roll') -> (terrain, roll', piece_corners index)) cDEFAULT_HEXES
+in let relevant_hexes = List.filter (fun (_, roll', _)-> roll=roll') hexes_with_corners
+in List.fold_left 
+(fun acc (terrain, roll, corners) -> 
+match resource_of_terrain terrain with 
+| Some resource -> List.fold_left (fun acc' point -> resources_of_corners resource acc' point structures) acc corners
+| None -> acc
+ ) plist relevant_hexes
+ 
+let players_who_need_discards plist = 
+	List.filter (fun (_, (inventory, _), _) -> (sum_cost inventory) > cMAX_HAND_SIZE ) plist 
+		
+let doAction (action: action) (plist: player list) (board: board) : player list = 
 	match action with 
  			| RollDice -> let x = random_roll () in  
- 			if x = cROBBER_ROLL
- 			then failwith "do robber shit"
- 			else hex_of_roll x  
+ 						if x = cROBBER_ROLL
+ 						then failwith "do robber shit"
+ 						else generate_resources x board plist 
             | MaritimeTrade (maritimetrade) -> failwith "maritimetrade"
             | DomesticTrade (trade) -> failwith "trade"
             | BuyBuild (build) -> failwith "BuyBuild"
--- release/own_util.ml
+++ release/own_util.ml
@@ -1,5 +1,5 @@
-(* returns hex index based on roll. Extremel surprised this wasn't implemented.*)
-let hex_of_roll roll = match roll with 
+(* returns hex index based on roll.*)
+let piece_of_roll roll = match roll with 
 | 2 -> [17]
 | 3 -> [8;15]
 | 4 -> [3;10]
@@ -12,7 +12,16 @@ let hex_of_roll roll = match roll with
 | 12 -> [1]
 | _ -> failwith "invalid roll"
 
+(* returns a list from a cost tuple*)
 let list_of_resources (b,w,o,g,l) = [b;w;o;g;l]
+
+(* inverse of method above. 
+takes a list and returns a cost tuple *)
 let resources_of_list l = match l with 
 | b::w::o::g::l::[] -> (b,w,o,g,l)
 | _ -> failwith "incorrect type of list in resources_of_list"
+
+(* adds two cost tuples*)
+let sum_of_two_costs (b,w,o,g,l) (c,x,p,h,m) = 
+	((b+c),(w+x), (o+p), (g+h), (l+m))
+
